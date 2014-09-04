5dbda53a11d9bccdc948a804715f2432bb5ee4f7
Zachary M. Jones <zmj@zmjones.com>
2014-09-04 15:30:13 -0400

    progress on #1, classification now working
    need to do regression and survival

--- R/pd.R
+++ R/pd.R
@@ -1,20 +1,21 @@
 #' Partial dependence using random forests
 #'
 #' Calculates the partial dependence of the response on an arbitrary dimensional set of predictors
-#' from a fitted random forest object from the Party or randomForest packages
+#' from a fitted random forest object from the Party, randomForest, or randomForestSRC packages
 #'
 #' @author Zachary M. Jones, \email{zmj@@zmjones.com}
 #'
 #' @importFrom parallel mclapply
 #' @importFrom party cforest cforest_control
 #' @importFrom randomForest randomForest
+#' @importFrom randomForestSRC rfsrc
 #' 
 #' @param fit an object of class 'RandomForest-class' returned from \code{cforest}, or an object
 #' of class 'randomForest' returned from \code{randomForest}
 #' @param df the dataframe used to fit the model, if the model is a party object of class 'RandomForest'
 #' this option can be omitted and the dataframe will be extracted from the object
 #' @param var a character vector of the predictors of interest, which must match the input
-#' matrix in the call to \code{cforest}
+#' matrix in the call to \code{cforest}, \code{randomForest}, or \code{randomForestSRC}
 #' @param surv logical, indicates whether or not the response is right-censored
 #' @param cores indicates the number of cores to use. parallelization occurs in the prediction
 #' on the grid of possible values taken by all combinations of `var`
@@ -27,24 +28,26 @@
 #' @examples
 #' require(randomForest)
 #' require(party)
+#' require(randomForestSRC)
 #' require(parallel)
 #' data(iris)
 
 #' fit_rf <- randomForest(Species ~ ., iris)
 #' fit_pt <- cforest(Species ~ ., iris, controls = cforest_control(mtry = 2))
-
-#' fit_rf <- randomForest(Species ~ ., iris)
-#' fit_pt <- cforest(Species ~ ., iris, controls = cforest_control(mtry = 2))
-
+#' fit_rfsrc <- rfsrc(Species ~ ., iris)
+#' 
 #' pd_rf <- partial_dependence(fit_rf, iris, "Petal.Width", detectCores())
 #' pd_pt <- partial_dependence(fit_pt, iris, "Petal.Width", detectCores())
-
+#' pd_rfsrc <- partial_dependence(fit_rfsrc, iris, "Petal.Width", detectCores())
+#' 
 #' pd_int_rf <- partial_dependence(fit_rf, iris, c("Petal.Width", "Sepal.Length"), detectCores())
 #' pd_int_pt <- partial_dependence(fit_pt, iris, c("Petal.Width", "Sepal.Length"), detectCores())
+#' pd_int_rfsrc <- partial_dependence(fit_rfsrc, iris, c("Petal.Width", "Sepal.Length"), detectCores())
 #'
 #' @export
-partial_dependence <- function(fit, var, surv = FALSE, cores = 1, ...) {
-    df <- data.frame(get("input", fit@data@env), get("response", fit@data@env))
+partial_dependence <- function(fit, df, var, surv = FALSE, cores = 1, ...) {
+    if (any(class(fit) == "RandomForest"))
+        df <- data.frame(get("input", fit@data@env), get("response", fit@data@env))
     rng <- lapply(var, function(x) ivar_points(df, x))
     rng <- expand.grid(rng)
     pred <- mclapply(1:nrow(rng), function(i) {
@@ -57,7 +60,10 @@ partial_dependence <- function(fit, var, surv = FALSE, cores = 1, ...) {
             pred <- sapply(weights(fit), function(w) median(df[, ncol(df)][rep(1:nrow(df), w)]))
             c(rng[i, 1:ncol(rng)], mean(pred))
         } else {
-            pred <- table(predict(fit, newdata = df))
+            if (any(class(fit) == "rfsrc"))
+                pred <- table(predict(fit, newdata = df, outcome = "test")$class.oob)
+            else
+                pred <- table(predict(fit, newdata = df))
             c(rng[i, 1:ncol(rng)], names(pred)[pred == max(pred)])
         }
     }, mc.cores = cores)
--- README.md
+++ README.md
@@ -7,19 +7,20 @@ This package allows you to easily calculate the partial dependence of an arbitra
 ```{r}
 require(randomForest)
 require(party)
+require(randomForestSRC)
 require(parallel)
 require(edarf)
 data(iris)
 
 fit_rf <- randomForest(Species ~ ., iris)
 fit_pt <- cforest(Species ~ ., iris, controls = cforest_control(mtry = 2))
-
-fit_rf <- randomForest(Species ~ ., iris)
-fit_pt <- cforest(Species ~ ., iris, controls = cforest_control(mtry = 2))
+fit_rfsrc <- rfsrc(Species ~ ., iris)
 
 pd_rf <- partial_dependence(fit_rf, iris, "Petal.Width", detectCores())
 pd_pt <- partial_dependence(fit_pt, iris, "Petal.Width", detectCores())
+pd_rfsrc <- partial_dependence(fit_rfsrc, iris, "Petal.Width", detectCores())
 
 pd_int_rf <- partial_dependence(fit_rf, iris, c("Petal.Width", "Sepal.Length"), detectCores())
 pd_int_pt <- partial_dependence(fit_pt, iris, c("Petal.Width", "Sepal.Length"), detectCores())
+pd_int_rfsrc <- partial_dependence(fit_rfsrc, iris, c("Petal.Width", "Sepal.Length"), detectCores())
 ```
