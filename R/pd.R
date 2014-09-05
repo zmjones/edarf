472701efb577699e4973240f50390d7523aa67e3
Zachary M. Jones <zmj@zmjones.com>
2014-09-05 00:58:59 -0400

    combined misc.R with pd.R
    a pointless separation

--- R/pd.R
+++ R/pd.R
@@ -108,7 +108,7 @@ partial_dependence <- function(fit, df, var, cores = 1, cutoff = 10) {
     colnames(pred)[ncol(pred)] <- "pred"
     return(pred)
 }
-
+#'
 #' Calculates the fitted values of observations with one or more variables set to a particular value
 #' and returns a summary of these fitted values: means for regression and survival analysis and
 #' the modal category for classification,
@@ -157,4 +157,22 @@ pd_inner <- function(fit, df, var, rng, type, i) {
                 pred <- sample(pred, 1) ## guess if class proportions are all equal
             c(rng[i, 1:ncol(rng)], pred)
         }
+}
+#'
+#' Creates a prediction vector for variables to decrease computation time
+#'
+#' @param df the dataframe used to fit the random forest, extracted from the fitted object
+#' @param x a character vector of length 1 indicating the variable in `df` to be extracted
+#' @param cutoff an integer indicating the maximal length of the vector to be used for prediction
+#' 
+#' @return a vector of unique values taken by `x` of length < `cutoff`
+#' 
+#' @export
+ivar_points <- function(df, x, cutoff = 10) {
+    rng <- unique(df[, x])
+    rng <- rng[!is.na(rng)]
+    if (length(rng) > cutoff & !is.factor(df[, x]))
+        rng <- seq(min(rng), max(rng), length.out = cutoff)
+    class(rng) <- class(df[, x])
+    return(rng)
 }
