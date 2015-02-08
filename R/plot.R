139b409fc631fa02afaf1a5addd516296946e82b
Zachary M. Jones <zmj@zmjones.com>
2015-02-08 13:44:04 -0500

    silly styling changes

--- R/plot.R
+++ R/plot.R
@@ -20,43 +20,41 @@
 #' pd <- partial_dependence(fit, iris, "Petal.Width")
 #' plot(pd)
 #' @export
-
 plot.pd <- function(pd, title = NULL, facet_var = NULL) {
   atts <- attributes(pd)
   # One predictor Plots
-  if(!atts$interaction) {
-    if(!atts$prob) {
+  if (!atts$interaction) {
+    if (!atts$prob) {
       # Numeric Y or Majority class
       df <- data.frame(x = pd[, 1], y = pd[, 2])
       p <- ggplot(df, aes(x, y))
       p <- p + geom_point() + geom_line()
       p <- p + labs(y = paste("Predicted", colnames(pd)[2]), x = colnames(pd)[1], 
-                    title = title) 
+                    title = title)
       p <- p + theme_bw()
-      p  
+      p
     } else {
       # Predicted probabilities
       df <- melt(pd, id.vars = 1)
-      if(is.null(xlab)) xlab <- colnames(pd)[1]
+      if (is.null(xlab)) xlab <- colnames(pd)[1]
       colnames(df) <- c("x", "Class", "Probability")
       p <- ggplot(df, aes(x = x, y = Probability, fill = Class))
-      p <- p + geom_area(position = "fill") 
+      p <- p + geom_area(position = "fill")
       #p <- p + scale_fill_brewer(palette = "Set5")
       p <- p + scale_fill_grey()
       p <- p + theme_bw() + labs(x = xlab, title = title)
       p
     }
-    
   } else {
     # Interaction Plots
     assert_that(ncol(pd) == 3)
-    if(atts$prob) 
+    if (atts$prob)
       stop("Interaction plot for predicted class probabilities not implemented")
-    if(class(pd[, 3]) == "numeric") {
+    if (class(pd[, 3]) == "numeric") {
       if(is.null(facet_var)) {
         n_unique <- apply(pd[, 1:2], 2, function(x) length(unique(x)))
         facet_var <- names(which.min(n_unique))
-      }  
+      }
       pd[, facet_var] <- as.factor(pd[, facet_var])
       plot_var <- colnames(pd)[which(colnames(pd)[-3] != facet_var)]
       y <- colnames(pd)[3]
@@ -70,5 +68,4 @@ plot.pd <- function(pd, title = NULL, facet_var = NULL) {
       stop("Interaction plot for discrete outcomes not implemented")
     }
   }
-
 }
