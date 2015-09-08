db134501a7ac12efec588a75cf261e727220f56e
flinder <fridolin.linder@gmail.com>
2015-09-08 14:30:57 -0400

    I honestly have no idea

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -10,26 +10,34 @@ using namespace Rcpp;
 double wmean(arma::rowvec x, arma::colvec w);
 RcppExport SEXP edarf_wmean(SEXP xSEXP, SEXP wSEXP) {
 BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< arma::rowvec >::type x(xSEXP);
-    Rcpp::traits::input_parameter< arma::colvec >::type w(wSEXP);
-    __result = Rcpp::wrap(wmean(x, w));
-    return __result;
+    SEXP __sexp_result;
+    {
+        Rcpp::RNGScope __rngScope;
+        Rcpp::traits::input_parameter< arma::rowvec >::type x(xSEXP );
+        Rcpp::traits::input_parameter< arma::colvec >::type w(wSEXP );
+        double __result = wmean(x, w);
+        PROTECT(__sexp_result = Rcpp::wrap(__result));
+    }
+    UNPROTECT(1);
+    return __sexp_result;
 END_RCPP
 }
 // vestl
 arma::mat vestl(int n, int ntree, arma::mat pd_membership, arma::rowvec yvar, arma::mat inbag);
 RcppExport SEXP edarf_vestl(SEXP nSEXP, SEXP ntreeSEXP, SEXP pd_membershipSEXP, SEXP yvarSEXP, SEXP inbagSEXP) {
 BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< int >::type n(nSEXP);
-    Rcpp::traits::input_parameter< int >::type ntree(ntreeSEXP);
-    Rcpp::traits::input_parameter< arma::mat >::type pd_membership(pd_membershipSEXP);
-    Rcpp::traits::input_parameter< arma::rowvec >::type yvar(yvarSEXP);
-    Rcpp::traits::input_parameter< arma::mat >::type inbag(inbagSEXP);
-    __result = Rcpp::wrap(vestl(n, ntree, pd_membership, yvar, inbag));
-    return __result;
+    SEXP __sexp_result;
+    {
+        Rcpp::RNGScope __rngScope;
+        Rcpp::traits::input_parameter< int >::type n(nSEXP );
+        Rcpp::traits::input_parameter< int >::type ntree(ntreeSEXP );
+        Rcpp::traits::input_parameter< arma::mat >::type pd_membership(pd_membershipSEXP );
+        Rcpp::traits::input_parameter< arma::rowvec >::type yvar(yvarSEXP );
+        Rcpp::traits::input_parameter< arma::mat >::type inbag(inbagSEXP );
+        arma::mat __result = vestl(n, ntree, pd_membership, yvar, inbag);
+        PROTECT(__sexp_result = Rcpp::wrap(__result));
+    }
+    UNPROTECT(1);
+    return __sexp_result;
 END_RCPP
 }
