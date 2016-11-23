36fabd7a29e580d4b700af627ee6a0aa28191334
Zachary M. Jones <zmj@zmjones.com>
2016-11-23 07:48:06 -0500

    bugfix in predict.fun for rfsrc

--- pkg/R/imp.R
+++ pkg/R/imp.R
@@ -99,8 +99,7 @@ variable_importance.rfsrc <- function(fit, vars,
     "y" = fit$yvar.names,
     "nperm" = nperm,
     "model" = fit,
-    "predict.fun" = function(object, newdata)
-      predict(object, newdata)[[if (is.factor(fit$yvar)) "class" else "predicted"]],
+    "predict.fun" = function(object, newdata) predict(object, newdata)[["predicted"]],
     ...
   )
 
